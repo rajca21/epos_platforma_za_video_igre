1141642e89709d3c7b9bb218421d48e03257b698
Rista <risticlazarsmd@gmail.com>
2022-02-08 18:26:19 +0100

    8.2.2022

---
+++ tim_1/gameA.js
@@ -0,0 +1,9 @@
+function startGame() {
+    let playDiv = document.getElementById("play")
+    let difficultyDiv = document.getElementById("difficulty")
+    let gameCanvas = document.getElementById("canvas");
+    
+    playDiv.style.display = "none";
+    difficultyDiv.style.display="none";
+    gameCanvas.style.display = "block";
+}
