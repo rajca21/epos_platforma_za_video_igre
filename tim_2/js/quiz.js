8bba3c9db03b5495ac7024ad6d7696ac99f267ba
VasilijeGogic29 <vaskegogic00@gmail.com>
2022-02-17 20:30:27 +0100

    kviz

--- tim_2/js/quiz.js
+++ tim_2/js/quiz.js
@@ -1,22 +1,14 @@
 const quizData = [
     {
-<<<<<<< HEAD
-        question: "Which language runs in a web browser?",
-        a: "1996",
-        b: "C",
-        c: "Python",
-        d: "javascript",
-=======
-        question: "Koji je najviši vrh na svetu?",
+        question: "1. Koji je najviši vrh na svetu?",
         a: "Kilimandžaro",
         b: "K2",
         c: "Monblan",
         d: "Mont Everest",
->>>>>>> 2ad924313a4ddd688ed752ccd06727e2898a3c36
         correct: "d",
     },
     {
-        question: "Ko je otac geografije?",
+        question: "2. Ko se smatra ocem geografije?",
         a: "Eratosten",
         b: "Arhimed",
         c: "Epikur",
@@ -24,7 +16,7 @@ const quizData = [
         correct: "a",
     },
     {
-        question: "Koji je glavni grad Letonije?",
+        question: "3. Koji je glavni grad Letonije?",
         a: "Viljnus",
         b: "Talin",
         c: "Riga",
@@ -32,7 +24,7 @@ const quizData = [
         correct: "c",
     },
     {
-        question: "Ko je naš najpoznatiji geograf?",
+        question: "4. Ko je naš najpoznatiji geograf?",
         a: "Milutin Milanković",
         b: "Jovan Cvijić",
         c: "Mihailo Petrović Alas",
@@ -40,23 +32,143 @@ const quizData = [
         correct: "b",
     },
     {
-        question: "Erozija je?",
+        question: "5. Koji je glavni grad Etiopije?",
+        a: "Mogadišu",
+        b: "Daka",
+        c: "Adis Abeba",
+        d: "Ništa od navedenog",
+        correct: "c",
+    },
+    {
+        question: "6. Erozija je?",
         a: "Razaranje postojećeg tla",
         b: "Akumuliranje zemlje",
         c: "Formiranje kriptodepresije",
         d: "Ništa od navedenog",
         correct: "a",
     },
+    {
+        question: "7. Koja je reka od ponuđenih bifurkacija?",
+        a: "Pek",
+        b: "Sava",
+        c: "Topčiderka",
+        d: "Nerodimka",
+        correct: "d",
+    },
+    {
+        question: "8. Koji je glavni grad Moldavije?",
+        a: "Minsk",
+        b: "Kišinjev",
+        c: "Tbilisi",
+        d: "Talin",
+        correct: "b",
+    },
+    {
+        question: "9. Koja je zvanična valuta u Japanu?",
+        a: "Japanski jen",
+        b: "Japanski juan",
+        c: "Japanski dolar",
+        d: "Japanski dinar",
+        correct: "a",
+    },
+    {
+        question: "10. Kako se zvao socijalni sistem koji je bio zastupljen u JAR tokom XX veka?",
+        a: "Apartrejs",
+        b: "Apartman",
+        c: "Aparthejd",
+        d: "Aparting",
+        correct: "c",
+    },
+    {
+        question: "11. Koja je treća najmnogoljudnija zemlja an svetu?",
+        a: "Kina",
+        b: "Brazil",
+        c: "Indija",
+        d: "SAD",
+        correct: "d",
+    },
+    {
+        question: "12. Na kom kontinentu se nalazi pustinja Atakama?",
+        a: "Južna Amerika",
+        b: "Afrika",
+        c: "Severna Amerika",
+        d: "Azija",
+        correct: "a",
+    },
+    {
+        question: "13. U kojoj zemlji se nalazi planina Leotar?",
+        a: "Srbija",
+        b: "Slovenija",
+        c: "Bosna i Hercegovina",
+        d: "Crna Gora",
+        correct: "c",
+    },
+    {
+        question: "14. Koji je glavni grad Perua?",
+        a: "Lima",
+        b: "LaPaz",
+        c: "Santijago",
+        d: "Havana",
+        correct: "a",
+    },
+    {
+        question: "15. Kako se zove vrh Golije?",
+        a: "Tornik",
+        b: "Mačkov Kamen",
+        c: "Jankov Kamen",
+        d: "Babin Zub",
+        correct: "c",
+    },
+    {
+        question: "16. Koliko kilometara približno iznosi obim Ekvatora?",
+        a: "~120 000",
+        b: "~40 000",
+        c: "~70 000",
+        d: "~10 000",
+        correct: "b",
+    },
+    {
+        question: "17. Koji je najveći kontinent?",
+        a: "Evropa",
+        b: "Afrika",
+        c: "Azija",
+        d: "Severna Amerika",
+        correct: "c",
+    },
+    {
+        question: "18. Koji je glavni grad Jordana?",
+        a: "Baku",
+        b: "Rijad",
+        c: "Islamabad",
+        d: "Aman",
+        correct: "d",
+    },
+    {
+        question: "19. Koje ostrvo ima najveću površinu na svetu?",
+        a: "Borneo",
+        b: "Grenland",
+        c: "Madagaskar",
+        d: "Sumatra",
+        correct: "b",
+    },
+    {
+        question: "20. Koja je minimalna visina da bi uzvišenje bilo planina?",
+        a: "1000m",
+        b: "700m",
+        c: "500m",
+        d: "250m",
+        correct: "c",
+    },
+    
+
 
 
 ];
 
-quizData[0].a.src = '../pocetna.jpeg'
-
 const quiz= document.getElementById('quiz')
 const answerEls = document.querySelectorAll('.answer')
 const questionEl = document.getElementById('question')
-var a_text = document.getElementById('a_text')
+const a_text = document.getElementById('a_text')
 const b_text = document.getElementById('b_text')
 const c_text = document.getElementById('c_text')
 const d_text = document.getElementById('d_text')
@@ -108,16 +220,13 @@ submitBtn.addEventListener('click', () => {
        if(currentQuiz < quizData.length) {
            loadQuiz()
        } else {
-           quiz.innerHTML = `
-           <h2>You answered ${score}/${quizData.length} questions correctly</h2>
+        quiz.innerHTML = `
+           <h2>Svaka čast! Odgovorili ste tačno na ${score}/${quizData.length} pitanja!</h2>
 
-<<<<<<< HEAD
-           <button onclick="location.reload()"> Ajmo ponovo :) </button>
-=======
-           <button onclick="location.reload()">Ponovi</button>
->>>>>>> 2ad924313a4ddd688ed752ccd06727e2898a3c36
-           `
-        
+            <button onclick="location.reload()">Ponovi</button>
+            <button onclick="window.location.href='./pocetna.html'">Završi</button>
+            `
+           
        }
     }
 })
